73db0532fb2a8d0a038ad8f5a4f8e7b057b8479b
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 22:20:32 +0100

    fix tests

--- test.js
+++ test.js
@@ -2,11 +2,10 @@ import test from 'tape';
 import twitterMentions from './index';
 import tokens from 'twitter-tokens';
 
-test('npmProfileDownloads', { timeout: 30000 }, ({ equal, assert, end } = t) => {
-  twitterMentions(tokens, '662248638287110144', (err, res) => {
+test('npmProfileDownloads', { timeout: 30000 }, ({ equal, end } = t) => {
+  twitterMentions(tokens, '424125604925956096', (err, res) => {
     if (err) throw err;
-    console.log(res)
-    // equal('424196654758375425', last(res).id_str);
+    equal(res.length, 20, 'show fetch proper amount of mentions');
     end();
   });
 });
