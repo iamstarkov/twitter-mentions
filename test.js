c35f9b11b6e49495248befc618a7188407275b4a
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 16:41:15 +0100

    add tests

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-import { equal } from 'assert';
+import test from 'tape';
 import twitterMentions from './index';
+import tokens from 'twitter-tokens';
 
-it('should twitterMentions', () =>
-  equal(twitterMentions('unicorns'), 'unicorns'));
-
-it('should twitterMentions invalid input', () =>
-  equal(twitterMentions(), undefined));
+test('npmProfileDownloads', { timeout: 30000 }, ({ equal, assert, end } = t) => {
+  twitterMentions(tokens, '662248638287110144', (err, res) => {
+    if (err) throw err;
+    console.log(res)
+    // equal('424196654758375425', last(res).id_str);
+    end();
+  });
+});
