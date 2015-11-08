ef311aae0f5af3cad9412a4c44ead4e9f6952adb
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 22:08:56 +0100

    implement thing

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import Twitter from 'twit';
-import R, { last, concat, propEq, slice, findIndex, isEmpty } from 'ramda';
+import Bignum from 'bn.js';
+import { last, concat, propEq, slice, findIndex, merge, isEmpty } from 'ramda';
 
-function bignumDec(i) {
-  return (new bignum(i).sub(new bignum('1'))).toString();
+function bignumDec(number) {
+  return (new Bignum(number).sub(new Bignum('1'))).toString();
 }
 
 function getNextMentionsOptions(options, mentions) {
-  if (R.isEmpty(mentions)) return options;
-  return R.merge(options, { max_id: bignumDec(R.last(mentions).id_str) });
+  if (isEmpty(mentions)) return options;
+  return merge(options, { max_id: bignumDec(last(mentions).id_str) });
 }
 
 function accumulate(get, options, lastMentionToGet, mentions, cb) {
-  const isTarget = R.propEq('id_str', lastMentionToGet);
+  const isTarget = propEq('id_str', lastMentionToGet);
   const findTargetIndex = findIndex(isTarget);
   const nextMentionsOptions = getNextMentionsOptions(options, mentions);
   get(nextMentionsOptions, (err, res) => {
     if (err) return cb(err);
-    if (R.isEmpty(res)) {
+    if (isEmpty(res)) {
       return cb(new Error('Target mention is too far away'));
     }
     const accumulatedMentions = concat(mentions, res);
@@ -32,4 +33,4 @@ export default function getMentions(tokens, lastMentionToGet, cb) {
   const get = client.get.bind(client, 'statuses/mentions_timeline');
   const options = { trim_user: true, count: 200 };
   return accumulate(get, options, lastMentionToGet, [], cb);
-};
+}
