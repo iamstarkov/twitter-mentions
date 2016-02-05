ae59afd0e0d76a5f63581b968362a82804b1552b
andrepolischuk <andre.polischuk@gmail.com>
2016-02-05 12:25:32 +0300

    small refactoring

--- test.js
+++ test.js
@@ -4,14 +4,14 @@ import tokens from 'twitter-tokens';
 import { last } from 'ramda';
 import dec from 'bignum-dec';
 
-test('twitterMentions', { timeout: 30000 }, ({ equal, end } = t) => {
+test('twitterMentions', { timeout: 30000 }, ({ equal, end }) => {
   twitterMentions(tokens, '424125604925956096').then(mentions => {
     equal(mentions.length, 19, 'proper mention’s number');
     end();
   });
 });
 
-test('twitterMentions including since_id', { timeout: 30000 }, ({ equal, end } = t) => {
+test('twitterMentions including since_id', { timeout: 30000 }, ({ equal, end }) => {
   twitterMentions(tokens, dec('424125604925956096')).then(mentions => {
     equal(mentions.length, 20, 'proper mention’s number');
     equal(last(mentions).id_str, '424125604925956096', 'including since_id');
