fca6df1f4eebfc6429e76c1baaaa97d25f672a00
andrepolischuk <andre.polischuk@gmail.com>
2016-02-05 12:08:16 +0300

    promisify

--- index.js
+++ index.js
@@ -7,20 +7,19 @@ const getNextOptions = (options, mentions) =>
     ? options
     : merge(options, { max_id: pipe(last, prop('id_str'), dec)(mentions) });
 
-function accumulate(get, options, mentions, cb) {
+function accumulate(get, options, mentions) {
   const nextOptions = getNextOptions(options, mentions);
-  get(nextOptions, (err, res) => {
-    if (err) return cb(err);
+  return get(nextOptions).then(res => {
     const accumulatedMentions = concat(mentions, res);
     return (isEmpty(res))
-        ? cb(null, accumulatedMentions)
-        : accumulate(get, nextOptions, accumulatedMentions, cb);
+        ? accumulatedMentions
+        : accumulate(get, nextOptions, accumulatedMentions);
   });
 }
 
-export default function getMentions(tokens, sinceId, cb) {
+export default function getMentions(tokens, sinceId) {
   const client = new Twitter(tokens);
   const get = client.get.bind(client, 'statuses/mentions_timeline');
   const options = { trim_user: true, count: 200, since_id: sinceId };
-  return accumulate(get, options, [], cb);
+  return accumulate(get, options, []);
 }
--- test.js
+++ test.js
@@ -5,16 +5,14 @@ import { last } from 'ramda';
 import dec from 'bignum-dec';
 
 test('twitterMentions', { timeout: 30000 }, ({ equal, end } = t) => {
-  twitterMentions(tokens, '424125604925956096', (err, mentions) => {
-    if (err) throw err;
+  twitterMentions(tokens, '424125604925956096').then(mentions => {
     equal(mentions.length, 19, 'proper mention’s number');
     end();
   });
 });
 
 test('twitterMentions including since_id', { timeout: 30000 }, ({ equal, end } = t) => {
-  twitterMentions(tokens, dec('424125604925956096'), (err, mentions) => {
-    if (err) throw err;
+  twitterMentions(tokens, dec('424125604925956096')).then(mentions => {
     equal(mentions.length, 20, 'proper mention’s number');
     equal(last(mentions).id_str, '424125604925956096', 'including since_id');
     end();
