9930d0e2c58098408f56e362e2a7c0f46fdc7ddf
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-08 02:46:00 +0100

    update tests

--- test.js
+++ test.js
@@ -6,14 +6,14 @@ import dec from 'bignum-dec';
 
 test('twitterMentions', { timeout: 30000 }, ({ equal, end }) => {
   twitterMentions(tokens, '424125604925956096').then(mentions => {
-    equal(mentions.length, 19, 'proper mention’s number');
+    equal(mentions.length, 20, 'proper mention’s number');
     end();
   });
 });
 
 test('twitterMentions including since_id', { timeout: 30000 }, ({ equal, end }) => {
   twitterMentions(tokens, dec('424125604925956096')).then(mentions => {
-    equal(mentions.length, 20, 'proper mention’s number');
+    equal(mentions.length, 21, 'proper mention’s number');
     equal(last(mentions).id_str, '424125604925956096', 'including since_id');
     end();
   });
